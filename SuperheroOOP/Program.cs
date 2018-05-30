9b65e9f51ca143386e63f796bd36eaef1c249f72
JohnLaubenthal <laubenthaljohn@yahoo.com>
2018-05-30 13:03:50 -0400

    first commit

---
+++ SuperheroOOP/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperheroOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SuperHero George = new SuperHero(135, 900, "Overwhelming", "You will never know!");
+            George.Power();
+            George.Tortise();
+            George.Unkown();
+            George.Mind();
+
+
+        }
+
+
+
+    }
+}
